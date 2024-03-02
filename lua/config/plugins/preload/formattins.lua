c732ae7eaecc0833c38f4405356f3f367a0c3563
grChad <bgos-jesus@hotmail.com>
2024-03-01 21:03:26 -0500

    refactor: delete plugin null-ls

--- lua/config/plugins/preload/formattins.lua
+++ lua/config/plugins/preload/formattins.lua
@@ -1,68 +1,65 @@
 return {
    {
-      'jose-elias-alvarez/null-ls.nvim',
-      lazy = false,
-      config = function()
-         local status, null_ls = pcall(require, 'null-ls')
-
-         if not status then
-            return
-         end
-
-         local augroup_format = vim.api.nvim_create_augroup('Format', { clear = true })
-
-         null_ls.setup({
-            debug = false,
-
-            sources = {
-               null_ls.builtins.diagnostics.eslint_d.with({
-                  -- stylua: ignore
-                  condition = function(utils)
-                     return utils.root_has_file({
-                        '.eslintrc.js', '.eslintrc.cjs', '.eslintrc.yaml', '.eslintrc.yml', '.eslintrc.json',
-                     })
-                  end,
-               }),
-
-               null_ls.builtins.formatting.eslint_d.with({
-                  -- stylua: ignore
-                  condition = function(utils)
-                     return utils.root_has_file({
-                        '.eslintrc.js', '.eslintrc.cjs', '.eslintrc.yaml', '.eslintrc.yml', '.eslintrc.json',
-                     })
-                  end,
-               }),
-               null_ls.builtins.formatting.stylua,
-               null_ls.builtins.formatting.prettierd.with({
-                  -- stylua: ignore
-                  filetypes = {
-                     'html', 'yaml', 'markdown', 'json', 'javascript', 'javascriptreact',
-                     'typescript', 'typescriptreact', 'svelte', 'vue',
-                  },
-                  env = {
-                     PRETTIERD_DEFAULT_CONFIG = vim.fn.expand(
-                        '~/.config/nvim/lua/config/static/prettierrc.json'
-                     ),
-                  },
-               }),
-               null_ls.builtins.formatting.taplo,
-               null_ls.builtins.formatting.black,
-               null_ls.builtins.formatting.latexindent,
-            },
-
-            on_attach = function(client, _)
-               if client.server_capabilities.documentFormattingProvider then
-                  vim.api.nvim_clear_autocmds({ buffer = 0, group = augroup_format })
-                  vim.api.nvim_create_autocmd('BufWritePre', {
-                     group = augroup_format,
-                     buffer = 0,
-                     callback = function()
-                        vim.lsp.buf.format({ timeout_ms = 2000 })
-                     end,
-                  })
-               end
+      'stevearc/conform.nvim',
+      event = { 'BufWritePre' },
+      cmd = { 'ConformInfo' },
+      keys = {
+         {
+            -- Customize or remove this keymap to your liking
+            '<leader>fi',
+            function()
+               require('conform').format({ async = true, lsp_fallback = true })
             end,
-         })
+            mode = '',
+            desc = 'Format buffer',
+         },
+      },
+      -- Everything in opts will be passed to setup()
+      opts = {
+         -- Define your formatters
+         formatters_by_ft = {
+            ['javascript'] = { 'prettierd' },
+            ['javascriptreact'] = { 'prettierd' },
+            ['typescript'] = { 'prettierd' },
+            ['typescriptreact'] = { 'prettierd' },
+            ['vue'] = { 'prettierd' },
+            ['svelte'] = { 'prettierd' },
+            ['html'] = { 'prettierd' },
+            ['json'] = { 'prettierd' },
+            ['jsonc'] = { 'prettierd' },
+            ['yaml'] = { 'prettierd' },
+            ['markdown'] = { 'prettierd' },
+            ['markdown.mdx'] = { 'prettierd' },
+            ['graphql'] = { 'prettierd' },
+            ['handlebars'] = { 'prettierd' },
+
+            ['lua'] = { 'stylua' },
+            ['toml'] = { 'taplo' },
+            ['python'] = { 'black' },
+            ['latex'] = { 'latexindent' },
+         },
+         -- Set up format-on-save
+         format_on_save = { timeout_ms = 500, lsp_fallback = true },
+         -- Customize formatters
+         formatters = {
+            shfmt = {
+               prepend_args = { '-i', '2' },
+            },
+            eslint_d = {
+               condition = function(ctx)
+                  return vim.fs.find({
+                     '.eslintrc.js',
+                     '.eslintrc.cjs',
+                     '.eslintrc.yaml',
+                     '.eslintrc.yml',
+                     '.eslintrc.json',
+                  }, { path = ctx.filename, upward = true })[1]
+               end,
+            },
+         },
+      },
+      init = function()
+         vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
       end,
    },
 }
