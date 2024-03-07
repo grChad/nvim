bc66e522cf78f9e4f463ebbae546bba5037d9ceb
grChad <bgos-jesus@hotmail.com>
2024-03-07 10:34:37 -0500

    feat: add file root_dir to prettierd

--- lua/config/plugins/preload/formattins.lua
+++ lua/config/plugins/preload/formattins.lua
@@ -5,7 +5,6 @@ return {
       cmd = { 'ConformInfo' },
       keys = {
          {
-            -- Customize or remove this keymap to your liking
             '<leader>fi',
             function()
                require('conform').format({ async = true, lsp_fallback = true })
@@ -14,52 +13,93 @@ return {
             desc = 'Format buffer',
          },
       },
-      -- Everything in opts will be passed to setup()
-      opts = {
-         -- Define your formatters
-         formatters_by_ft = {
-            ['javascript'] = { 'prettierd' },
-            ['javascriptreact'] = { 'prettierd' },
-            ['typescript'] = { 'prettierd' },
-            ['typescriptreact'] = { 'prettierd' },
-            ['vue'] = { 'prettierd' },
-            ['svelte'] = { 'prettierd' },
-            ['html'] = { 'prettierd' },
-            ['json'] = { 'prettierd' },
-            ['jsonc'] = { 'prettierd' },
-            ['yaml'] = { 'prettierd' },
-            ['markdown'] = { 'prettierd' },
-            ['markdown.mdx'] = { 'prettierd' },
-            ['graphql'] = { 'prettierd' },
-            ['handlebars'] = { 'prettierd' },
-
-            ['lua'] = { 'stylua' },
-            ['toml'] = { 'taplo' },
-            ['python'] = { 'black' },
-            ['latex'] = { 'latexindent' },
-         },
-         -- Set up format-on-save
-         format_on_save = { timeout_ms = 500, lsp_fallback = true },
-         -- Customize formatters
-         formatters = {
-            shfmt = {
-               prepend_args = { '-i', '2' },
-            },
-            eslint_d = {
-               condition = function(ctx)
-                  return vim.fs.find({
-                     '.eslintrc.js',
-                     '.eslintrc.cjs',
-                     '.eslintrc.yaml',
-                     '.eslintrc.yml',
-                     '.eslintrc.json',
-                  }, { path = ctx.filename, upward = true })[1]
-               end,
-            },
-         },
-      },
+      -- opts = {
+      --    -- Define your formatters
+      --    formatters_by_ft = {
+      --       ['javascript'] = { 'prettierd' },
+      --       ['javascriptreact'] = { 'prettierd' },
+      --       ['typescript'] = { 'prettierd' },
+      --       ['typescriptreact'] = { 'prettierd' },
+      --       ['vue'] = { 'prettierd' },
+      --       ['svelte'] = { 'prettierd' },
+      --       ['html'] = { 'prettierd' },
+      --       ['json'] = { 'prettierd' },
+      --       ['jsonc'] = { 'prettierd' },
+      --       ['yaml'] = { 'prettierd' },
+      --       ['markdown'] = { 'prettierd' },
+      --       ['markdown.mdx'] = { 'prettierd' },
+      --       ['graphql'] = { 'prettierd' },
+      --       ['handlebars'] = { 'prettierd' },
+      --
+      --       ['lua'] = { 'stylua' },
+      --       ['toml'] = { 'taplo' },
+      --       ['python'] = { 'black' },
+      --       ['latex'] = { 'latexindent' },
+      --    },
+      --    format_on_save = { timeout_ms = 500, lsp_fallback = true },
+      --    formatters = { -- Customize formatters
+      --       shfmt = {
+      --          prepend_args = { '-i', '2' },
+      --       },
+      --       prettierd = {
+      --          -- condition = function(ctx)
+      --          --    return vim.fs.basename(ctx.filename) ~= 'biome.json'
+      --          -- end,
+      --          cwd = require('conform.util').root_file({ '.prettierrc.json', 'package.json' }),
+      --          require_cwd = true,
+      --       },
+      --    },
+      -- },
       init = function()
          vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+      end,
+      config = function()
+         require('conform').setup({
+            formatters_by_ft = {
+               ['javascript'] = { 'prettierd' },
+               ['javascriptreact'] = { 'prettierd' },
+               ['typescript'] = { 'prettierd' },
+               ['typescriptreact'] = { 'prettierd' },
+               ['vue'] = { 'prettierd' },
+               ['svelte'] = { 'prettierd' },
+               ['html'] = { 'prettierd' },
+               ['json'] = { 'prettierd' },
+               ['jsonc'] = { 'prettierd' },
+               ['yaml'] = { 'prettierd' },
+               ['markdown'] = { 'prettierd' },
+               ['markdown.mdx'] = { 'prettierd' },
+               ['graphql'] = { 'prettierd' },
+               ['handlebars'] = { 'prettierd' },
+
+               ['lua'] = { 'stylua' },
+               ['toml'] = { 'taplo' },
+               ['python'] = { 'black' },
+               ['latex'] = { 'latexindent' },
+            },
+            format_on_save = { timeout_ms = 500, lsp_fallback = true },
+            formatters = { -- Customize formatters
+               shfmt = {
+                  prepend_args = { '-i', '2' },
+               },
+               prettierd = {
+                  cwd = require('conform.util').root_file({
+                     '.prettierrc',
+                     '.prettierrc.json',
+                     '.prettierrc.yml',
+                     '.prettierrc.yaml',
+                     '.prettierrc.json5',
+                     '.prettierrc.js',
+                     '.prettierrc.mjs',
+                     '.prettierrc.cjs',
+                     '.prettierrc.toml',
+                     'prettier.config.js',
+                     'prettier.config.mjs',
+                     'prettier.config.cjs',
+                  }),
+                  require_cwd = true,
+               },
+            },
+         })
       end,
    },
 }
