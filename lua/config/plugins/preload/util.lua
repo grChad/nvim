b066a04dc798cbe7c3c961f24878648ff9266eeb
grChad <bgos-jesus@hotmail.com>
2024-03-25 10:02:22 -0500

    delete: inc-rename plugin

--- lua/config/plugins/preload/util.lua
+++ lua/config/plugins/preload/util.lua
@@ -1,13 +1,6 @@
 return {
    -- dependencies
    {
-      'smjonas/inc-rename.nvim',
-      cmd = 'IncRename',
-      config = function()
-         require('inc_rename').setup()
-      end,
-   },
-   {
       'nvim-tree/nvim-web-devicons',
       lazy = true,
       config = function()
