f202bd72832137d4b20f29b9d9c372f4fde9f4d1
grChad <bgos-jesus@hotmail.com>
2024-12-25 13:59:59 -0500

    chore: move lazydev.nvim to /autoComplete.lua

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -43,21 +43,6 @@ return {
    },
 
    {
-      'folke/lazydev.nvim',
-      ft = 'lua',
-      cmd = 'LazyDev',
-      opts = {
-         library = {
-            { path = 'luvit-meta/library', words = { 'vim%.uv' } },
-            { path = 'LazyVim', words = { 'LazyVim' } },
-            { path = 'lazy.nvim', words = { 'LazyVim' } },
-         },
-      },
-   },
-   -- Manage libuv types with lazy. Plugin will never be loaded
-   { 'Bilal2453/luvit-meta' },
-
-   {
       'akinsho/toggleterm.nvim',
       version = '*',
       cmd = 'ToggleTerm',
