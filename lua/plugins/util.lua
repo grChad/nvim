38846e65c2c16bf719d0921897168bf41df5dc01
grChad <bgos-jesus@hotmail.com>
2024-12-25 13:58:47 -0500

    feat: edit utils.nvim and nvim-web-devicons

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -1,18 +1,16 @@
 return {
    -- dependencies
    {
-      'grChad/grUtils.nvim',
+      'grChad/utils.nvim',
       dev = true,
       lazy = false,
       config = function()
-         local grUtils = require('gr-utils')
+         local gr_utils = require('gr-utils')
 
-         grUtils.statuscolumn.setup({
-            -- stylua: ignore
-            ignore_fold_ft = {
-               'text', 'markdown', 'mdx', 'help', 'qf', 'diff', 'fugitiveblame', 'alpha',
-               'mason', 'lspinfo', 'TelescopePrompt',
-            },
+         gr_utils.statuscolumn.setup({
+            without_fold = { 'text', 'markdown', 'mdx' },
+            -- disabled = {},
+            -- add_with_fold = {},
          })
       end,
    },
@@ -22,53 +20,22 @@ return {
       config = function()
          require('nvim-web-devicons').setup({
             override_by_filename = {
-               ['.gitignore'] = {
-                  icon = '󰊢',
-                  color = '#f1502f',
-                  name = 'Gitignore',
-               },
-               ['.prettierrc.json'] = {
-                  icon = '',
-                  color = '#BF85BF',
-                  name = 'PrettierRC',
-               },
-               ['.prettierrc.js'] = {
-                  icon = '',
-                  color = '#BF85BF',
-                  name = 'PrettierRC',
-               },
-               ['package-lock.json'] = {
-                  icon = '',
-                  color = '#c0b13a',
-                  name = 'FileLock',
-               },
-               ['lazy-lock.json'] = {
-                  icon = '',
-                  color = '#c0b13a',
-                  name = 'FileLock',
-               },
-               ['biome.json'] = {
-                  icon = '󰔶',
-                  color = '#60A5FA',
-                  name = 'Biome',
-               },
+               ['.gitignore'] = { icon = '󰊢', color = '#f1502f', name = 'Gitignore' },
+               ['.prettierrc.json'] = { icon = '', color = '#BF85BF', name = 'PrettierRC' },
+               ['.prettierrc.js'] = { icon = '', color = '#BF85BF', name = 'PrettierRC' },
+               ['package.json'] = { icon = '', color = '#8bc34a', name = 'Package' },
+               ['package-lock.json'] = { icon = '', color = '#85BD44', name = 'FileLock' },
+               ['lazy-lock.json'] = { icon = '', color = '#c0b13a', name = 'FileLock' },
+               ['biome.json'] = { icon = '󰔶', color = '#60A5FA', name = 'Biome' },
             },
             override_by_extension = {
-               ['astro'] = {
-                  icon = '',
-                  color = '#F76A02',
-                  name = 'Astro',
-               },
-               ['toml'] = {
-                  icon = '',
-                  color = '#9C4221',
-                  name = 'Toms',
-               },
-               ['gd'] = {
-                  icon = '',
-                  color = '#478cbf',
-                  name = 'Godot',
-               },
+               ['astro'] = { icon = '', color = '#F76A02', name = 'Astro' },
+               ['toml'] = { icon = '', color = '#9C4221', name = 'Toms' },
+               ['gd'] = { icon = '', color = '#478cbf', name = 'Godot' },
+               ['tsx'] = { icon = '󰜈', color = '#0E83C6', name = 'Typescriptreact' },
+               ['jsx'] = { icon = '󰜈', color = '#cbcb41', name = 'Javascriptreact' },
+               ['json'] = { icon = '', color = '#df9828', name = 'Json' },
+               ['sh'] = { icon = '', color = '#A98383', name = 'Shell' },
             },
          })
       end,
