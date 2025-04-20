8ff7ce00efe216ce2b5f72ddbfdeb8530a676fad
grChad <bgos-jesus@hotmail.com>
2025-04-20 10:43:24 -0500

    chore: delete lspconfig dependence

--- lsp/stylelint_lsp.lua
+++ lsp/stylelint_lsp.lua
@@ -1,6 +1,3 @@
--- NOTE: Todavia hay una dependencia de 'lspconfig'
-local util = require('lspconfig.util')
-
 local root_file = {
    '.stylelintrc',
    '.stylelintrc.mjs',
@@ -14,7 +11,29 @@ local root_file = {
    'stylelint.config.js',
 }
 
-root_file = util.insert_package_json(root_file, 'stylelint')
+local iswin = vim.uv.os_uname().version:match('Windows')
+
+---@param config_files string[]
+---@param field string
+---@param fname string
+local insert_package_json = function(config_files, field, fname)
+   local path = vim.fn.fnamemodify(fname, ':h')
+   local root_with_package = vim.fs.dirname(vim.fs.find('package.json', { path = path, upward = true })[1])
+
+   if root_with_package then
+      -- Solo agregar package.json si contiene el parametro del campo
+      local path_sep = iswin and '\\' or '/'
+      for line in io.lines(root_with_package .. path_sep .. 'package.json') do
+         if line:find(field) then
+            config_files[#config_files + 1] = 'package.json'
+            break
+         end
+      end
+   end
+   return config_files
+end
+
+root_file = insert_package_json(root_file, 'stylelint')
 
 ---@type vim.lsp.Config
 return {
