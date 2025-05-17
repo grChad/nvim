9a42f76971615d699b92603ac8e6a17e1ab74858
grChad <bgos-jesus@hotmail.com>
2025-05-16 22:12:36 -0500

    chore: format file and deleted lines unnecessary

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,25 +1,9 @@
--- :20 'LuaSnip'
---     'friendly-snippets'
--- :52 'nvim-cmp'
---     'cmp-nvim-lsp',
---     'cmp-buffer',
---     'cmp-path',
---     'cmp_luasnip',
---     'cmp-emoji',
--- :141 'nvim-autopairs',
--- :170 'nvim-ts-autotag'
--- :176 'mini.surround',
--- :195 'nvim-ts-context-commentstring',
--- :203 'Comment.nvim',
-
 return {
    {
       'kylechui/nvim-surround',
       version = '*',
       event = 'VeryLazy',
-      config = function()
-         require('nvim-surround').setup()
-      end,
+      config = function() require('nvim-surround').setup() end,
    },
 
    {
@@ -47,8 +31,6 @@ return {
       version = '*',
       cmd = 'ToggleTerm',
       keys = require('core.key_plugins').toggleterm,
-      config = function()
-         require('toggleterm').setup({ size = 10 })
-      end,
+      config = function() require('toggleterm').setup({ size = 10 }) end,
    },
 }
